d19b8f7849d06c6a3e8f3f5e3adc4c9409516daa
mar-formanite <251788947@formanite.fccollege.edu.pk>
2025-11-09 00:44:19 +0500

    added faculty code

--- src/people/Faculty.java
+++ src/people/Faculty.java
@@ -1,4 +1,30 @@
 package people;
 
-public class Faculty {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Faculty extends Person{
+    private double salary;
+    private List<String> assignedCourses = new ArrayList<>();
+
+    public Faculty(String name, String id, String email, String phone,double salary) {
+        super(name, id, email, phone);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    protected void setSalary(double salary) {this.salary = salary;}
+    public  void assignCourse(String course){
+        assignedCourses.add(courseCode);
+    }
+    @Override
+    public String getRole(){
+        return "Faculty";
+    }
+    @Override
+    public String toString(){
+        return getName() + " - "+ getRole() + "| Courses:" + assignedCourses;
+    }
 }
